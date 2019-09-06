97feb6ec5f9f27cb2b76039890f6bf067e2cf304
irisjitomo <irisjitomo@eyahoo.com>
2019-09-06 13:45:19 -0400

    Mvp met

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -7,3 +7,23 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// THis should pass
+
+describe('App', () => {
+  it('want to see if H1 has a certain string', () => {
+    const simulatedDOM = rtl.render(<App />);
+    const h1 = simulatedDOM.queryByText(/Advanced React Sprint Challenge/i);
+    expect(h1).toBeInTheDocument();
+  })
+})
+
+//This should fail
+
+describe('App', () => {
+  it('want to see if H2 has a certain string', () => {
+    const simulatedDOM = rtl.render(<App />);
+    const h2 = simulatedDOM.queryByText(/Hello/i);
+    expect(h2).toBeInTheDocument();
+  })
+})
